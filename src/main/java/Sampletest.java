7944fe6fecc53ed648911ffd4d6feed915793c3e
Anubhav <in-anubhav.srivastava@in-anubhavsrivastava.local>
2019-05-19 12:04:44 +0530

    added a program to add two number

---
+++ src/main/java/Sampletest.java
@@ -0,0 +1,12 @@
+public class Sampletest {
+    public static void main(String[] args) {
+
+        int num1 = 100;
+        int num2 =50;
+        int sum =num1 + num2;
+
+        System.out.println(sum) ;
+
+
+    }
+}
