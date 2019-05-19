0223ce0ff66b07b28fcebaa5d3c560ed322a6e31
Anubhav <in-anubhav.srivastava@in-anubhavsrivastava.local>
2019-05-19 12:50:44 +0530

    Nishi added a program to add string

---
+++ src/main/java/StringLearn.java
@@ -0,0 +1,11 @@
+public class StringLearn {
+    public static void main(String[] args) {
+        String str1 = "Nishi";
+        String str2 = "Anubhav";
+        String str3 = " And ";
+        String str4 = str1 + str3 + str2;
+
+        System.out.println(str4);
+
+    }
+}
